ef881832d6ef6e2bd0ec490e5bd142a6239f0e82
Lee Valverde <73706919+leevalverde@users.noreply.github.com>
2021-08-04 18:21:26 +0800

    Updated example to v14.2.2

---
+++ VB/E1984/MyTreeListSerializeHelper.vb
@@ -0,0 +1,59 @@
+﻿Imports DevExpress.Utils.Serializing
+Imports DevExpress.XtraTreeList
+Imports System.IO
+
+Namespace E1984
+    Public Module MyTreeListSerializeHelper
+        Public DefaultFileName As String = "test.xml"
+        Public SelectedTreeList As TreeList
+
+        Public Sub SaveTreeList()
+            SaveTreeList(SelectedTreeList, DefaultFileName)
+        End Sub
+
+        Public Sub RestoreTreeList()
+            RestoreTreeList(SelectedTreeList, DefaultFileName)
+        End Sub
+
+        Private Sub SaveTreeList(ByVal treeList As TreeList, ByVal fileName As String)
+            treeList.SaveLayoutToXml(fileName)
+            SaveConditions(treeList, "Conditions" & fileName)
+        End Sub
+
+        Private Sub SaveConditions(ByVal treeList As TreeList, ByVal fileName As String)
+            Dim serializer As XmlXtraSerializer = New XmlXtraSerializer()
+
+            Using stream As FileStream = New FileStream(fileName, FileMode.OpenOrCreate)
+
+                Try
+                    Dim collection As TreeListFormatConditions = New TreeListFormatConditions(treeList.FormatConditions)
+                    serializer.SerializeObject(collection, stream, "FormatConditions")
+                Finally
+                    stream.Close()
+                End Try
+            End Using
+        End Sub
+
+        Private Sub RestoreTreeList(ByVal treeList As TreeList, ByVal fileName As String)
+            treeList.RestoreLayoutFromXml(fileName)
+            RestoreConditions(treeList, "Conditions" & fileName)
+        End Sub
+
+        Private Sub RestoreConditions(ByVal treeList As TreeList, ByVal fileName As String)
+            If Not File.Exists(fileName) Then Return
+            Dim serializer As XmlXtraSerializer = New XmlXtraSerializer()
+
+            Using stream As FileStream = New FileStream(fileName, FileMode.Open)
+
+                Try
+                    Dim collection As TreeListFormatConditions = New TreeListFormatConditions()
+                    serializer.DeserializeObject(collection, stream, "FormatConditions")
+                    collection.Conditions.CopyToStyleFormats(treeList.FormatConditions)
+                    treeList.Refresh()
+                Finally
+                    stream.Close()
+                End Try
+            End Using
+        End Sub
+    End Module
+End Namespace
